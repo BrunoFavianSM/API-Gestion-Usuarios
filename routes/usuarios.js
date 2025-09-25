9e7f45301eb69fb071e866cbe9a282fa23cf1a7f
Bruno Saldarriaga <brunofavian20@gmail.com>
2025-09-25 11:56:22 -0500

    Primera subida solo metodo GET

---
+++ routes/usuarios.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { body, validationResul } = require('express-validator');
+const router = express.Router();
+
+const {
+    obtenerUsuarios
+} = require('../controllers/usuarioController');
+
+//RUTAS
+router.get('/',obtenerUsuarios);
+
+module.exports = router;
